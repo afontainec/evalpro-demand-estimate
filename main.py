51d97109fbbb65b4b2175912b9ef5a46a8efbbdf
Antonio Fontaine <afontaine@uc.cl>
2017-04-26 19:39:01 -0300

    calculate the age piramid for a given year

---
+++ main.py
@@ -0,0 +1,36 @@
+import area
+import Population
+
+zones = []
+
+def setup():
+    divisions = init_divisions()
+    potential_client_prob = calculate_potential_client()
+    iterate()
+
+
+
+def iterate():
+    print 'iterar'
+    for zone in zones:
+        zone.adjust_parameters()
+        zone.calculate_customers()
+
+def init_divisions():
+    Population.init_death_rates()
+    Population.init_birth_rates()
+    Population.init_age_piramid_to(2013);
+
+
+# NOT IMPLEMENTED: should return a data strcture with the probability that someone will take t minutes to get to the pharmacy
+def calculate_potential_client():
+    return 0.5
+
+
+
+
+
+
+
+
+setup()
