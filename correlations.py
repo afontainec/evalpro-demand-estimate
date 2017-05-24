38416fd3bf4bad9cece5ec0a24701d6f358aca2d
Antonio Fontaine <afontaine@uc.cl>
2017-05-24 19:11:25 -0400

    Correlations

--- correlations.py
+++ correlations.py
@@ -2,7 +2,45 @@ import Zones
 import Population
 import Scenarios
 import Printer
+import csv
 from Global import INITIAL_YEAR, FINAL_YEAR, YEARS
 
+porcentage = {}
+with open('patients/raw/total.csv', 'rU') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        porcentage[row['zone']] = row['porcentage']
 
-Population.get_pyramid(1, 2017)
+private_travel = {}
+actual_travel = {}
+
+with open('data/travel_time.csv', 'rU') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        private_travel[row['Unidad Vecinal']] = row['Private']
+        actual_travel[row['Unidad Vecinal']] = row['Actual']
+
+poberty = {}
+with open('data/poberty.csv', 'rU') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        poberty[row['zone']] = row['poberty_level']
+
+
+y_axis = []
+x_axis_private = []
+x_axis_actual = []
+x_axis_poberty = []
+
+
+for zone in Zones.get():
+    z = str(zone)
+    y_axis.append(porcentage[z])
+    x_axis_private.append(private_travel[z])
+    x_axis_actual.append(actual_travel[z])
+    x_axis_poberty.append(poberty[z])
+
+
+Printer.line_graph(x_axis_private, y_axis, './correlations/time_to_private_pharmacy.png')
+Printer.line_graph(x_axis_actual, y_axis, './correlations/time_to_actual_pharmacy.png')
+Printer.line_graph(x_axis_poberty, y_axis, './correlations/poberty.png')
